80f609b554ac37a8771c4672b63bdf17e4f026f8
kim <kim@kim.(none)>
2014-07-18 18:58:25 +0400

    Fixed bugs

--- main.py
+++ main.py
@@ -9,10 +9,33 @@ from parsellvm import *
 from sh import osc
 import argparse
 
+def parsing_args():
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+					description='''
+	Build utils, run tests and compare results
+
+	Examples:
+	  -b aarch aarch64 gcc49 
+	  -r aarch aarch64 gcc49
+	  -c gcc49 week ago
+	  -c gcc49 31.06.14 14.06.15
+	  -c gcc49 gcc48
+	  -c gcc49 aarch64 i586
+					''')
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('-b','--build', help='build <> <> <>',required=False, nargs='+')
+	group.add_argument('-r','--runtests',help='run tests <> <> <>', required=False, nargs='+')
+	group.add_argument('-c','--compare',help='compare <> <> <>', required=False, nargs='+')
+	args = parser.parse_args()
+
+	print args.build
+	###
+
 print "===TestanalIZEN==="
 
 one = parseone()
-one.parse("./one.txt") one.show()
+one.parse("./one.txt") 
+one.show()
 
 two = parsetwo()
 two.parse("./gcc_res.txt")
@@ -37,28 +60,6 @@ llvm.parse("./gcc.sum")
 llvm.show()
 
 parsing_args();
-
-def parsing_args():
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-					description='''
-	Build utils, run tests and compare results
-
-	Examples:
-	  -b aarch aarch64 gcc49 
-	  -r aarch aarch64 gcc49
-	  -c gcc49 week ago
-	  -c gcc49 31.06.14 14.06.15
-	  -c gcc49 gcc48
-	  -c gcc49 aarch64 i586
-					''')
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('-b','--build', help='build <> <> <>',required=False, nargs='+')
-	group.add_argument('-r','--runtests',help='run tests <> <> <>', required=False, nargs='+')
-	group.add_argument('-c','--compare',help='compare <> <> <>', required=False, nargs='+')
-	args = parser.parse_args()
-
-	print args.build
-	###
 
 class main:
 	def __init__(self):
