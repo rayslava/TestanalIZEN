4b31bf5ea42fdbc0ecf830352ce7b90b8f5ff380
kim <kim@kim.(none)>
2014-07-22 17:23:54 +0400

    Added parsing args class, Fixed debug flag

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ repo=""
 arch=""
 package=""
 debug = False
+
 @contextlib.contextmanager
 def cd(path):
    old_path = os.getcwd()
@@ -29,49 +30,55 @@ def cd(path):
    finally:
        os.chdir(old_path)
 
-def parsing_args():
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-					description='''
-	Build utils, run tests and compare results
+class parsing_args:
+	def __init__(self):
+		pass
+	def parse(self):
+		global debug
+		parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+						description='''
+		Build utils, run tests and compare results
 
-	Examples:
-	  -b aarch aarch64 gcc49 
-	  -r aarch aarch64 gcc49
-	  -c gcc49 week ago
-	  -c gcc49 31.06.14 14.06.15
-	  -c gcc49 gcc48
-	  -c gcc49 aarch64 i586
-					''')
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('-b','--build', help='build <> <> <>',nargs='+')
-	group.add_argument('-r','--runtests',help='run tests <> <> <>', nargs='+')
-	group.add_argument('-c','--compare',help='compare <> <> <>', nargs='+')
-	parser.add_argument('-d','--debug',help='debug', action='store_true')
-	args = parser.parse_args()
-	if args.debug == True:
-		debug = True
-	if len(args.build) == 3:
-		print 'Osc build request..'
-		repo=args.build[0]
-		arch=args.build[1]
-		package=args.build[2]
-		if repo in repo_list and arch in arch_list and package in package_list: 
-			print 'Good input'
-			osc()
+		Examples:
+		  -b aarch aarch64 gcc49 -d
+		  -r aarch aarch64 gcc49
+		  -c gcc49 week ago
+		  -c gcc49 31.06.14 14.06.15
+		  -c gcc49 gcc48
+		  -c gcc49 aarch64 i586
+						''')
+		group = parser.add_mutually_exclusive_group(required=True)
+		group.add_argument('-b','--build', help='build <> <> <>',nargs='+')
+		group.add_argument('-r','--runtests',help='run tests <> <> <>', nargs='+')
+		group.add_argument('-c','--compare',help='compare <> <> <>', nargs='+')
+		parser.add_argument('-d','--debug',help='debug', action='store_true')
+		args = parser.parse_args()
+		if args.debug:
+			debug = True
+			print "Debug"
+		### OSC build request
+		if len(args.build) == 3:
+			print 'Osc build request..'
+			repo=args.build[0]
+			arch=args.build[1]
+			package=args.build[2]
+			if repo in repo_list and arch in arch_list and package in package_list: 
+				print 'Good input'
+				osc()
+			else:
+				print 'Bad input'
+				parser.print_help()
 		else:
-			print 'Bad input'
 			parser.print_help()
-	else:
-		parser.print_help()
-	print args.build
-	###
+		print args.build
+		###
 
 def osc():
 	print debug
         if os.path.exists(PROJECT+'/'+package):
                 with cd(PROJECT+"/"+package):
-                        print "Updating.."
                         if not debug:
+				print "Updating.."
 				subprocess.call(["osc", "update"])
         else:
                 print "Checkout.."
@@ -79,7 +86,7 @@ def osc():
         with cd(PROJECT+"/"+package):
                 print "Building.."
                 subprocess.call(["osc", "build", repo, arch])
-
+'''
 one = parseone()
 one.parse("./one.txt") 
 one.show()
@@ -105,8 +112,9 @@ print '===LLVM RESULTS==='
 llvm = parsellvm()
 llvm.parse("./gcc.sum")
 llvm.show()
-
-parsing_args();
+'''
+parsing_args_inst = parsing_args()
+parsing_args_inst.parse()
 
 class main:
 	def __init__(self):
