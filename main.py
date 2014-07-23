9e12621d0fe01f25a4534514bb7105aa6af50db0
kim <kim@kim.(none)>
2014-07-23 21:01:36 +0400

    Added Parse class

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ class Parsing_args(object):
 		group = argparser.add_mutually_exclusive_group(required=True)
 		group.add_argument('-b','--build', help="OSC co&build: -b osc 'REPOSITORY ARCH PACKAGE [OPTS]'\n",nargs='+')
 		group.add_argument('-r','--runtests',help='Run tests: -r PACKAGE') 
+		group.add_argument('-p','--parse',help='Parsing results: -p PACKAGE')
 		group.add_argument('-c','--compare',help='compare <> <> <>', nargs='+')
 		argparser.add_argument('-d','--debug',help='debug', action='store_true')
 		args = argparser.parse_args()
@@ -80,6 +81,10 @@ class Build(object):
 		else:
 			argparser.print_help()
 			sys.exit(0)
+		
+		if debug:
+			print 'build args:'
+			print args.build
 
 class Osc_build(Build):
 	global repo,arch,package
@@ -141,28 +146,109 @@ class Osc_build(Build):
 class Run_tests(object):
 	def __init__(self):
 		pass
-        ## TODO build expand
+        ## TODO tests expand
         #
         #  	
         @staticmethod
         def parse_args():
 		global argparser,args
 		if args.runtests == 'gcc':
-			print '===GCC49 test request==='
+			print '===GCC 4.9 test request==='
+			run_tests_gcc49 = Run_tests_gcc49()
+			run_tests_gcc49.start()
 		else:
 			print 'Bad input'
 			argparser.print_help()
 			sys.exit(0)
 
-		print 'runtests args: '
-		print args.runtests
+		if debug:
+			print 'runtests args: '
+			print args.runtests
 
 class Run_tests_gcc49(Run_tests):
 	def __init__(self):
 		super(Run_tests_gcc49, self).__init__()
+	
 	@staticmethod
-	def get_log():
-		pass
+	def start():
+		pass # TODO
+
+class Parse(object):
+	def __init__(self):
+        	pass
+	## TODO parse expand
+	#
+	#	
+	@staticmethod
+	def parse_args():
+		global argparser,args
+		if args.parse == 'gcc49':
+			print '===GCC 4.9 parse request==='
+			parse_gcc49 = Parse_gcc49()
+			parse_gcc49.start()
+		else:
+			print 'Bad input'
+                        argparser.print_help()
+                        sys.exit(0)
+
+		if debug:
+			print 'parse agrs'
+			print args.parse
+	
+class Parse_gcc49(Parse):
+	path="./libstdc++.sum"
+        pass_cnt = 0
+        xpass_cnt = 0
+        fail_cnt = 0
+        xfail_cnt = 0
+        unsupported_cnt= 0
+        error_cnt = 0
+        warning_cnt = 0	
+        def __init__(self):
+                super(Parse_gcc49, self).__init__()
+                self.pass_cnt = 0
+                self.xpass_cnt = 0
+                self.fail_cnt = 0
+                self.xfail_cnt = 0
+                self.unsupported_cnt= 0
+                self.error_cnt = 0
+                self.warning_cnt = 0
+	def start(self):
+                pass_regexp = re.compile('PASS\s*:\s*')
+                xpass_regexp = re.compile('XPASS\s*:\s*')
+                fail_regexp = re.compile('FAIL\s*:\s*')
+                xfail_regexp = re.compile('XFAIL\s*:\s*')
+                unsupported_regexp = re.compile('UNSUPPORTED\s*:\s*')
+                error_regexp = re.compile('ERROR\s*:\s*')
+                warning_regexp = re.compile('WARNING\s*:\s*')
+                with open(self.path) as f:
+                        for line in f:
+                                if pass_regexp.match(line):
+                                        self.pass_cnt += 1
+                                if fail_regexp.match(line):
+                                        self.fail_cnt += 1
+                                if xpass_regexp.match(line):
+                                        self.xpass_cnt += 1
+                                if fail_regexp.match(line):
+                                        self.xfail_cnt += 1
+                                if unsupported_regexp.match(line):
+                                        self.unsupported_cnt += 1
+                                if error_regexp.match(line):
+                                        self.error_cnt += 1
+                                if warning_regexp.match(line):
+                                        self.warning_cnt += 1
+		self.show()
+	
+        def show(self):
+		print '===GCC 4.9 TESTS RESULTS==='
+                print 'PASS: %d' % self.pass_cnt
+                print 'FAIL: %d' % self.fail_cnt
+                print 'XPASS: %d' % self.xpass_cnt
+                print 'XFAIL: %d' % self.xfail_cnt
+                print 'UNSUPPORTED: %d' % self.unsupported_cnt
+                print 'ERROR: %d' % self.error_cnt
+                print 'WARNING: %d' % self.warning_cnt
+
 '''
 one = parseone()
 one.parse("./one.txt") 
@@ -196,5 +282,7 @@ if args.build:
 	Build.parse_args()
 if args.runtests:
 	Run_tests.parse_args()
-		
+if args.parse:
+	Parse.parse_args()	
+
 print '====finish==='
